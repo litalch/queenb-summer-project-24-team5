d74efb52f8b0ed83c5db73c2f97bc23af4fc3207
Mike Vazan <mike.vazan@mail.huji.aac.il>
2024-09-14 19:25:45 +0300

    Import AuthContextProvider to index.js file, and structure for its use there. Awaits updates from other people.

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.css'; 
 import App from './App';
-import { DuckProvider } from './context/DuckContext';
+import { DuckProvider } from './context/DuckContext'; // This is from the rubber duck example. should probably delete this
 
+import { AuthContextProvider } from './context/AuthContext';
+
+
+// This is from the rubber duck example. We should probably adjust this...
+// The new bit is that '<AuthContextProvider>' wraps the content (duck) provider thing
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DuckProvider>
-      <App /> 
-    </DuckProvider>
+    <AuthContextProvider>
+      <DuckProvider>
+        <App /> 
+      </DuckProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
