9c9ed229e9d9bd31660a56e1b13d2ccf5a7c55d4
Mike Vazan <mike.vazan@mail.huji.aac.il>
2024-09-14 17:14:37 +0300

    Fill in try-catch for signup user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,14 +1,25 @@
 const User = require('../models/userModel')
-
+// we use this to interact with our users collections in our database
 
 // login user - only structure for now
 const loginUser = async (req, res) => {
     res.json({mssg: 'login user'})
 }
 
-// signup user - only structure for now
+// signup user 
 const signupUser = async (req, res) => {
-    res.json({mssg: 'signup user'})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.signup(email, password)
+
+        res.status(200).json({email, user}) // if theres is no error, User.signup returns a user object created in mongoDB
+    } catch (error) {
+        res.status(400).json({error: error.mssg})
+
+    }
+
+    // res.json({mssg: 'signup user'}) // this was here when I wanted to test API requests using Postman
 }
 
 // This allows us to export the above functions for use
