c7ea8c912302a8aa44dae653223ed8dd649ae9be
Mike Vazan <mike.vazan@mail.huji.aac.il>
2024-09-14 19:10:46 +0300

    Create AuthContext.js file in client/src/context folder, and handle login/logout state

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,32 @@
+import { createContext, useReducer } from 'react'
+
+export const AuthContext = createContext()
+
+
+// checks the action log-in/out / nothing, and returns data accordingly 
+export const authReducer = (state, action) => {
+    switch (action.type) { // we check which type of action we do - login/logout
+        case 'LOGIN':
+            return { user: action.payload } // if we log in, it returns something associated to the user
+        case 'LOGOUT':
+            return { user: null } // if we log out, it returns null
+        default:
+            return state // if there are no changes to the state, it returns the state
+    }
+}
+
+export const AuthContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(authReducer, {
+        user: null
+    })
+
+    // Every time the log-in/out state changes, this will be logged to the console (to help us keep track of the state)
+    console.log('AuthContext state: ', state)
+
+
+    return (
+        <AuthContext.Provider value={{...state, dispatch}}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
