14b2c789b4b7b6bc01b8193a1054725c5378403f
Mike Vazan <mike.vazan@mail.huji.aac.il>
2024-09-21 17:28:10 +0300

    Create logout hook

---
+++ client/src/hooks/useLogout.js
@@ -0,0 +1,18 @@
+import { useAuthContext } from "./useAuthContext"
+
+export const useLogout = () => {
+
+// we don't actully need to send a request to the backend. 
+// we only need to change the state and delete the token from the local storage
+const logout = () => { 
+    const {dispatch} = useAuthContext()
+
+    // The following removes user from storage
+    localStorage.removeItem('user')
+
+    // dispatch logout action
+    dispatch({type: 'LOGOUT'})
+}
+
+return {logout}
+}
