8fe85d513348a6a76407f6d122f57a8d68b8f815
shaiArtsi <artsi.shai21@gmail.com>
2024-09-22 15:10:28 +0300

    FilterMenu_GridItem

--- client/src/components/ItemsGrid/ItemsGrid.js
+++ client/src/components/ItemsGrid/ItemsGrid.js
@@ -2,25 +2,24 @@ import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './ItemsGrid.module.css'; 
+import FilterMenu from '../FilterMenu/FilterMenu';  
 
 const ItemsGrid = () => {
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([])
   const [loading, setLoading] = useState(false);
+  const [filters, setFilters] = useState({
+    categories: [],
+  });
 
   useEffect(() => {
     const getItems = async () => {
       setLoading(true);
       try {
-        const response = await api.get('/items/women');
-        console.log(response.data); 
-
+        const response = await api.get('api/items/women');
         const items = response.data.items || response.data;
-
-        if (!Array.isArray(items)) {
-          setData([items]);
-        } else {
-          setData(items);
-        }
+        setData(Array.isArray(items) ? items : [items]);
+        setFilteredData(items);
       } catch (error) {
         console.error('Failed to fetch items:', error);
       } finally {
@@ -31,12 +30,27 @@ const ItemsGrid = () => {
     getItems();
   }, []);
 
+  const handleFilterChange = (filters) => {
+    const { categories = [], gender = [], conditions = [] } = filters;
+  
+    const filteredData = data.filter((item) => {
+      const categoryMatch = categories.length === 0 || categories.includes(item.category);
+      const genderMatch = gender.length === 0 || gender.includes(item.gender);
+      const conditionMatch = conditions.length === 0 || conditions.includes(item.condition);
+  
+      return categoryMatch && genderMatch && conditionMatch;
+    });
+  
+    setFilteredData(filteredData);
+  };
+  
   return (
     <div className="container">
       {loading && <div>Loading...</div>}
-      {data && data.length > 0 ? (
+      {filteredData.length > 0 ? (
         <div className="justify-content-center">
-          {data.map((item) => (
+          <FilterMenu onFilterChange={handleFilterChange} />
+          {filteredData.map((item) => (
             <div className="card-container" key={item.id}>
               <div className="card">
                 <img src={item.imageUrl} className="card-img" alt={item.name} />
