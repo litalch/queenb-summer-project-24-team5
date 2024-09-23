c971cd7898f726584c29b360643ba12476765fff
liormary <liorms3@gmail.com>
2024-09-23 16:27:05 +0300

    links changes

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -2,13 +2,19 @@ import React from 'react';
 import styles from './Home.module.css';
 import womenswear from './Womenswear.png';
 import menswear from './Menswear.png';
+import { Link } from 'react-router-dom';
+
 
 
 const Home = () => {
   return (
     <div className={styles.home}>
-      <img src={womenswear} alt="Womenswear" />
-      <img src={menswear} alt="Menswear" />
+      <Link to= "/WomensCollection" className={styles.card}> 
+        <img src={womenswear} alt="Womenswear" />
+      </Link>
+      <Link to="/MensCollection" className={styles.card}> 
+        <img src={menswear} alt="Menswear" />
+      </Link>
     </div>
   );
 };
