89d0a112b9b163abc50f0e0d9bea54433a81934b
liormary <liorms3@gmail.com>
2024-09-25 21:14:31 +0300

    item details changes

--- client/src/components/ItemDetails/ItemDetails.js
+++ client/src/components/ItemDetails/ItemDetails.js
@@ -33,28 +33,27 @@ const ItemDetails = () => {
     fetchItem();
   }, [id]);
 
-  // לוג של המידע שמתקבל
-  useEffect(() => {
+
     console.log("Current Item State:", item); // לוג ה-state הנוכחי של item
-  }, [item]);
+
 
   return (
     <div className={styles.itemDetailContainer}>
       {loading && <div className={styles.loading}>Loading...</div>}
       {!item && !loading && <div className={styles.noItem}>Item doesn't exist</div>}
-      {!loading && item && (
+      { item && (
         <div className={styles.item}>
-          {item.imageUrl && (
-            <img src={item.imageUrl} alt={item.name || "Item Image"} className={styles.itemImg} />
+          {item.item.imageUrl && (
+            <img src={item.item.imageUrl} alt={item.imageUrl || "Item Image"} className={styles.itemImg} />
           )}
           <div className={styles.itemDet}>
-            <h2 className={styles.itemName}>{item.name}</h2>
-            <p className={styles.itemGender}>{item.gender}</p> 
-            <p className={styles.itemPrice}>{`${item.price}$`}</p>
-            <p className={styles.itemSize}>Size: ${item.size}</p>
-            <p className={styles.itemCondition}>{item.condition}</p> 
-            <p className={styles.itemDescription}>
-              {item.description || "No description available"}
+            <h2 className={styles.itemName}>{item.item.name}</h2>
+            <p className={styles.itemGender}>{item.item.gender}</p> 
+            <p className={styles.itemPrice}>{`${item.item.price}$`}</p>
+            <p className={styles.itemSize}>Size: {item.item.size}</p>
+            <p className={styles.itemCondition}>{item.item.condition}</p> 
+            <p className={styles.item.itemDescription}>
+              {item.item.description || "No description available"}
             </p>
             <FirstButton>Contact Seller</FirstButton>
           </div>
