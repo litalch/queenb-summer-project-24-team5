ab7a87a22ce1289936edeebfb7fb72421896f046
Mike Vazan <mike.vazan@mail.huji.aac.il>
2024-09-26 11:43:42 +0300

    managed to sign up, still debugging

--- server/routes/users.js
+++ server/routes/users.js
@@ -12,5 +12,10 @@ router.post('/login', loginUser)
 // signup route
 router.post('/signup', signupUser)
 
+// test
+router.post('/test', (req,res) => {
+    res.send('Hello from users route, post')
+})
+
 module.exports = router
 
